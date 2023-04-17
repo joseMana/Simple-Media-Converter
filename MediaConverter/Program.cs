7efb0fabc7cf1a7078b5bd3afe11331aea53ee77
Joseph Manangan <jmanangan@3cloudsolutions.com>
2023-04-17 22:31:03 +0800

    Add project files.

---
+++ MediaConverter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using NReco.VideoConverter;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var ffmpeg = new NReco.VideoConverter.FFMpegConverter();
+        ffmpeg.ConvertMedia(
+            "input.avi", null, 
+            "output.mp4", null, 
+            new ConvertSettings());
+    }
+}
